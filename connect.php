c86b762620fb67d6c3e2ec586b178257f58c45e1
luonggialuan <luonggialuan07402200@gmail.com>
2024-04-20 11:56:36 +0700

    Initial Project

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php
+$servername = "localhost";
+$username = "root"; 
+$password = ""; 
+$dbname = "web_comp1640"; 
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Failed" . $conn->connect_error);
+}
+?>
