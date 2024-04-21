80a2f48857949c3048a11b8299da58e0ac3c89a8
luonggialuan <luonggialuan07402200@gmail.com>
2024-04-21 22:44:29 +0700

    Revert "Update all"
    This reverts commit 5550c2c59caa21ae365d71b5d3db225e04102acd.

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "web_comp1640";
+$username = "root"; 
+$password = ""; 
+$dbname = "web_comp1640"; 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
--- public/src/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-$_SESSION = array();
-session_destroy();
-echo '<meta http-equiv="refresh" content="0;URL=?page=signin"/>';
-exit;
-?>
